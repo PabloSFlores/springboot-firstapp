91cacc6fe48d5f1f5874080c25eeaaebc9bc83b1
PabloSFlores <20213tn013@utez.edu.mx>
2023-02-26 20:10:29 -0600

    Cambio

--- firstapp/src/main/java/mx/edu/utez/firstapp/models/user/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package mx.edu.utez.firstapp.models.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findOneByUsername(String username);
-}
